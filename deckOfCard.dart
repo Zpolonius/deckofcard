1620cf61a41ebff3e645f4af97e60148dfcc0c18
zpolonius <zacharias@polonius.dk>
2023-05-01 20:18:22 +0200

    master

--- deckOfCard.dart
+++ deckOfCard.dart
@@ -1,8 +1,30 @@
-class Deck {
-late List<Card> cards;
+void main() {
+  var deck = Deck();
+  print(deck);
 }
 
-class Card{
-  late String rank;
-  late String suit;
+class Deck {
+  List<Card> cards = [];
+
+  Deck() {
+    var ranks = ['Ace', 'two', 'three', 'four', 'five'];
+    var suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        var card = Card(rank, suit);
+        cards.add(card);
+      }
+    }
+  }
+  @override
+  toString() {
+    return 'This is a deck';
+  }
+}
+
+class Card {
+  String rank;
+  String suit;
+  Card(this.rank, this.suit);
 }
